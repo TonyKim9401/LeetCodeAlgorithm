69afa8f221e35e94563681c86d4ae8d0fa6a72d9
Tony <87371627+TonyKim9401@users.noreply.github.com>
2024-06-04 03:54:21 +0900

    Time: 3 ms (5.37%), Space: 42.8 MB (5.26%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public int search(int[] nums, int target) {
+
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i); // num, index
+        }
+
+        int start = 0;
+        int end = nums.length - 1;
+        Arrays.sort(nums);
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (nums[mid] == target) {
+                return map.get(target);
+            } else if (nums[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
