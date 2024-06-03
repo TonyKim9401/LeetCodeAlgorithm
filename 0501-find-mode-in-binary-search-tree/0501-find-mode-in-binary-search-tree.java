d104cef331f54f368ebabc030418156f11555f05
Tony <87371627+TonyKim9401@users.noreply.github.com>
2024-06-04 07:14:09 +0900

    Time: 0 ms (100%), Space: 44.8 MB (65.31%) - LeetHub

--- 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.java
+++ 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.java
@@ -13,57 +13,40 @@
  *     }
  * }
  */
-import java.util.*;
-
 class Solution {
+
+    private int currentValue = 0;
+    private int currentCount = 0;
+    private int maxCount = 0;
+    private List<Integer> valueList = new ArrayList<>();
+
     public int[] findMode(TreeNode root) {
-        Map<Integer, Integer> map = new HashMap<>();
-        List<Integer> keyList = new ArrayList<>();
-        List<Integer> valList = new ArrayList<>();
-
-        if (root != null) {
-            if (map.containsKey(root.val)) {
-                int value = map.get(root.val);
-                map.put(root.val, value++);
-            } else {
-                map.put(root.val, 1);
-                keyList.add(root.val);
-            }
-            subMode(root.left, map, keyList);
-            subMode(root.right, map, keyList);
-        }
-
-        int max = 0;
-        for (int i = 0; i < keyList.size(); i++) {
-            int key = keyList.get(i);
-            max = Math.max(map.get(key), max);
-        }
-        for (int i = 0; i < keyList.size(); i++) {
-            int key = keyList.get(i);
-            if (max == map.get(key)) {
-                valList.add(key);
-            }
-        }
-
-        int[] result = new int[valList.size()];
-        for (int i = 0; i < valList.size(); i++) {
-            result[i] = valList.get(i);
+        
+        checkMode(root);
+        int listLength = valueList.size();
+        int[] result = new int[listLength];
+        for (int i = 0; i < listLength; i++) {
+            result[i] = valueList.get(i);
         }
         return result;
     }
 
-    public void subMode(TreeNode root, Map<Integer, Integer> map, List<Integer> keyList) {
-        if (root != null) {
-            if (map.containsKey(root.val)) {
-                int value = map.get(root.val);
-                value += 1;
-                map.put(root.val, value);
-            } else {
-                map.put(root.val, 1);
-                keyList.add(root.val);
-            }
-            subMode(root.left, map, keyList);
-            subMode(root.right, map, keyList);
+    public void checkMode(TreeNode node) {
+
+        if (node == null) return;
+
+        checkMode(node.left);
+
+        currentCount = (node.val == currentValue) ? currentCount + 1 : 1;
+        if (currentCount == maxCount) {
+            valueList.add(node.val);
+        } else if (currentCount > maxCount) {
+            maxCount = currentCount;
+            valueList.clear();
+            valueList.add(node.val);
         }
+        currentValue = node.val;
+
+        checkMode(node.right);
     }
 }
