6976b16a88f416fe4b108d0760e0693631c0c1d5
Tony <87371627+TonyKim9401@users.noreply.github.com>
2024-06-05 06:18:14 +0900

    Time: 1 ms (99%), Space: 41.5 MB (90.38%) - LeetHub

---
+++ 0139-word-break/0139-word-break.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        
+        int n = s.length();
+        boolean[] dp = new boolean[n + 1];
+        dp[0] = true;
+        int maxLength = 0;
+
+        for (String word : wordDict) {
+            maxLength = Math.max(maxLength, word.length());
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = i - 1; j >= Math.max(i - maxLength - 1, 0); j--) {
+                if (dp[j] && wordDict.contains(s.substring(j,i))) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+
+        return dp[n];
+    }
+}
