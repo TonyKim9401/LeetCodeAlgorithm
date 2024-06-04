ad16ffc07292e053746aff312e194af77038d5f5
Tony <87371627+TonyKim9401@users.noreply.github.com>
2024-06-04 11:09:30 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.java
+++ 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.java
@@ -18,15 +18,15 @@ class Solution {
     private int currentValue = 0;
     private int currentCount = 0;
     private int maxCount = 0;
-    private List<Integer> valueList = new ArrayList<>();
+    private List<Integer> maxValList = new ArrayList<>();
 
     public int[] findMode(TreeNode root) {
-        
+
         checkMode(root);
-        int valueLength = valueList.size();
-        int[] result = new int[valueLength];
-        for (int i = 0; i < valueLength; i++) {
-            result[i] = valueList.get(i);
+        int resultSize = maxValList.size();
+        int[] result = new int[resultSize];
+        for (int i = 0; i < resultSize; i++) {
+            result[i] = maxValList.get(i);
         }
         return result;
     }
@@ -39,14 +39,14 @@ class Solution {
 
         currentCount = (subRoot.val == currentValue) ? currentCount + 1 : 1;
         if (currentCount == maxCount) {
-            valueList.add(subRoot.val);
+            maxValList.add(subRoot.val);
         } else if (currentCount > maxCount) {
+            maxValList.clear();
             maxCount = currentCount;
-            valueList.clear();
-            valueList.add(subRoot.val);
+            maxValList.add(subRoot.val);
         }
-        currentValue = subRoot.val;
 
+        currentVal = subRoot.val;
         checkMode(subRoot.right);
     }
 }
