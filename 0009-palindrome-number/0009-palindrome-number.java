ae26e159f6c19a6df429a8258fb017d00170a1c3
Tony <87371627+TonyKim9401@users.noreply.github.com>
2024-06-09 02:55:42 +0900

    Time: 14 ms (8.35%), Space: 44 MB (30.64%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,20 +1,12 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        
         if (x < 0) return false;
 
-        int palNo = 0;
-        int size = (int) (Math.log10(x) + 1);
-        int target = x;
-
-        for (int i = 1; i <= size; i++) {
-            int rest = target % 10;
-            target /= 10;
-            
-            palNo *= 10;
-            palNo += rest;
+        String num = x + "";
+        StringBuilder sb = new StringBuilder();
+        for (int i = num.length() - 1; i >= 0; i--) {
+            sb.append(num.charAt(i));
         }
-
-        return (x == palNo);
+        return x == Long.valueOf(sb.toString());
     }
 }
