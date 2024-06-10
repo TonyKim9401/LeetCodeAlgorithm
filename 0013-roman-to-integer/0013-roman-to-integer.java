e8344e06c1a14984a977e08527338aeaed05c57e
Tony <87371627+TonyKim9401@users.noreply.github.com>
2024-06-10 12:12:52 +0900

    Time: 6 ms (30.31%), Space: 44.2 MB (92.44%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,38 +1,21 @@
 class Solution {
     public int romanToInt(String s) {
-        String[] exceptions = {"IV", "IX", "XL", "XC", "CD", "CM"};
-        Map<String, Integer> map = new HashMap<>();
-        map.put("IV", 4);
-        map.put("IX", 9);
-        map.put("XL", 40);
-        map.put("XC", 90);
-        map.put("CD", 400);
-        map.put("CM", 900);
+        Map<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
 
         int result = 0;
-        
-        for (int i = 0; i < exceptions.length; i++) {
-            if (s.contains(exceptions[i])) {
-                s = s.replaceFirst(exceptions[i], "");
-                result += map.get(exceptions[i]);
-                i--;
-            }
-        }
+
         for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == 'I') {
-                result += 1;
-            } else if (s.charAt(i) == 'V') {
-                result += 5;
-            } else if (s.charAt(i) == 'X') {
-                result += 10;
-            } else if (s.charAt(i) == 'L') {
-                result += 50;
-            } else if (s.charAt(i) == 'C') {
-                result += 100;
-            } else if (s.charAt(i) == 'D') {
-                result += 500;
-            } else if (s.charAt(i) == 'M') {
-                result += 1000;
+            if (i < s.length() - 1 && map.get(s.charAt(i)) < map.get(s.charAt(i+1))) {
+                result -= map.get(s.charAt(i));
+            } else {
+                result += map.get(s.charAt(i));
             }
         }
         return result;
