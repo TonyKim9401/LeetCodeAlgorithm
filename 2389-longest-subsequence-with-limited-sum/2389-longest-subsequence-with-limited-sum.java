f869d798b0b4d34c0ccfe6c65bf18a2eed2dd190
Tony <87371627+TonyKim9401@users.noreply.github.com>
2024-06-10 22:53:26 +0900

    Time: 6 ms (59.21%), Space: 44.3 MB (95.31%) - LeetHub

--- 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.java
+++ 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.java
@@ -1,32 +1,50 @@
 class Solution {
     public int[] answerQueries(int[] nums, int[] queries) {
+        
+        // 1 <= n <= 1000
+        // 1 <= m <= 1000
+        // 1 <= nums.length <= 10^6
+        // 1 <= queries.length <= 10^6 (10 power 6)
+
+        // declare int variables n, m based the given arrays named nums and queries
+
         int n = nums.length;
         int m = queries.length;
+
+        // sort nums array with ascending
         Arrays.sort(nums);
-        int[] result = new int[m];
-        for (int i = 1; i < nums.length; i++) {
-            nums[i] += nums[i - 1];
+        // [1,2,4,5];
+        // [1,1+2,1+2+4,1+2+4+5];
+        //   1,2,3,4 .... 
+        //-> [1,3,6,12];
+        for (int i = 1; i < n; i++) {
+            nums[i] += nums[i-1];
         }
 
+        int[] output = new int[m];
+
+        // use 2 for-loop to figure out the output.
         for (int i = 0; i < m; i++) {
             int q = queries[i];
 
             int start = 0;
             int end = n - 1;
-            while(start <= end) {
-                int mid = start + (end - start) / 2;
 
+            // declare int variables to adopt binary search
+            while (start <= end) {
+                int mid = start + (end - start) / 2;
+                
                 if (nums[mid] == q) {
-                    result[i] = mid + 1;
+                    output[i] = mid + 1;
                     break;
                 } else if (nums[mid] < q) {
-                    result[i] = mid + 1;
+                    output[i] = mid + 1;
                     start = mid + 1;
                 } else {
                     end = mid - 1;
                 }
             }
         }
-        return result;
+        return output;        
     }
 }
