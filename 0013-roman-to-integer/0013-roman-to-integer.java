d420daa6212ed8f05a946c90e70114d48b3bb67b
Tony <87371627+TonyKim9401@users.noreply.github.com>
2024-06-10 11:33:50 +0900

    Time: 10 ms (6.43%), Space: 44.6 MB (58.36%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,38 +1,38 @@
-import java.util.*;
-
 class Solution {
     public int romanToInt(String s) {
-        Map<String, Integer> exceptions = new HashMap<>();
-        exceptions.put("IV", 4);
-        exceptions.put("IX", 9);
-        exceptions.put("XL", 40);
-        exceptions.put("XC", 90);
-        exceptions.put("CD", 400);
-        exceptions.put("CM", 900);
+        String[] exceptions = {"IV", "IX", "XL", "XC", "CD", "CM"};
+        Map<String, Integer> map = new HashMap<>();
+        map.put("IV", 4);
+        map.put("IX", 9);
+        map.put("XL", 40);
+        map.put("XC", 90);
+        map.put("CD", 400);
+        map.put("CM", 900);
 
         int result = 0;
-        boolean check = true;
-        while (check) {
-            for (String key : exceptions.keySet()) {
-                check = false;
-                if (s.contains(key)) {
-                    result += exceptions.get(key);
-                    s = s.replace(key, "");
-                    check = true;
-                }
+        
+        for (int i = 0; i < exceptions.length; i++) {
+            if (s.contains(exceptions[i])) {
+                s = s.replaceFirst(exceptions[i], "");
+                result += map.get(exceptions[i]);
+                i--;
             }
         }
-
         for (int i = 0; i < s.length(); i++) {
-            char a = s.charAt(i);
-            switch(a) {
-                case 'I' -> result += 1;
-                case 'V' -> result += 5;
-                case 'X' -> result += 10;
-                case 'L' -> result += 50;
-                case 'C' -> result += 100;
-                case 'D' -> result += 500;
-                case 'M' -> result += 1000;
+            if (s.charAt(i) == 'I') {
+                result += 1;
+            } else if (s.charAt(i) == 'V') {
+                result += 5;
+            } else if (s.charAt(i) == 'X') {
+                result += 10;
+            } else if (s.charAt(i) == 'L') {
+                result += 50;
+            } else if (s.charAt(i) == 'C') {
+                result += 100;
+            } else if (s.charAt(i) == 'D') {
+                result += 500;
+            } else if (s.charAt(i) == 'M') {
+                result += 1000;
             }
         }
         return result;
