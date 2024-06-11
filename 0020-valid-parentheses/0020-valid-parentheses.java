ca0670c903366d1af6d951f603fedaad6af69f98
Tony <87371627+TonyKim9401@users.noreply.github.com>
2024-06-12 03:50:54 +0900

    Time: 1 ms (98.89%), Space: 41.4 MB (56.61%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,8 +1,7 @@
 class Solution {
     public boolean isValid(String s) {
-        if (s.length() % 2 == 1) return false;
-
         Stack<Character> stack = new Stack<>();
+
         for (char c : s.toCharArray()) {
             if (c == '(') {
                 stack.push(')');
