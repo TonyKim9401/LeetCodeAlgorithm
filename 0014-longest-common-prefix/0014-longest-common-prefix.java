97994c8e4d244416b4789b380062dcb0c1ea7827
Tony <87371627+TonyKim9401@users.noreply.github.com>
2024-06-12 03:46:23 +0900

    Time: 2 ms (28.29%), Space: 41.4 MB (57.41%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -6,6 +6,7 @@ class Solution {
         boolean check = true;
         StringBuilder output = new StringBuilder();
         for (int i = 0; i < shortest.length(); i++) {
+            if (!check) break;
             char target = shortest.charAt(i);
             for (int j = 1; j < strs.length; j++) {
                 if (target != strs[j].charAt(i)) check = false;
