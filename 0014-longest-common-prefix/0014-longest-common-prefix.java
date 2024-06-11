4d0407eabdf2d4ebc2b7365465dd37ea20034424
Tony <87371627+TonyKim9401@users.noreply.github.com>
2024-06-12 03:46:14 +0900

    Time: 1 ms (75.27%), Space: 41.5 MB (46.72%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -3,15 +3,12 @@ class Solution {
         Arrays.sort(strs, (o1, o2) -> o1.length() - o2.length());
 
         String shortest = strs[0];
-        StringBuilder output = new StringBuilder();
         boolean check = true;
+        StringBuilder output = new StringBuilder();
         for (int i = 0; i < shortest.length(); i++) {
             char target = shortest.charAt(i);
             for (int j = 1; j < strs.length; j++) {
-                if (target != strs[j].charAt(i)) {
-                    check = false;
-                    break;
-                }
+                if (target != strs[j].charAt(i)) check = false;
             }
             if (check) output.append(target);
         }
