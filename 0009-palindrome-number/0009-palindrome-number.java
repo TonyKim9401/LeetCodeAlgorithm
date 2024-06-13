01a19beab1d13b889b24300a2224c283095e8a65
Tony <87371627+TonyKim9401@users.noreply.github.com>
2024-06-13 23:08:35 +0900

    Time: 6 ms (37.26%), Space: 43.8 MB (67.68%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -2,22 +2,14 @@ class Solution {
     public boolean isPalindrome(int x) {
         
         // x range, -2^31 < x < 2^31-1
-        // Converts the given parameter from int type to String type
-        // for-loop to check x's each values until the half of the String
-        // "-121" -> false
-        // "10" -> false
-        // "121" -> true
 
         // check the given parameter x is less than 0 -> return false
         if (x < 0) return false;
 
+        // Converts the given parameter from int type to String type
         String xString = String.valueOf(x);
 
-        // 10 , length = 2
-        // for (int i = 0; i < 1 ; i++) {
-        //  if (xString.chartAt(0) != xString.charAt(1 - 0 - 1)) return false
-        //.            1           !=          0
-        //}
+        // for-loop to check x's each values until the half of the String
         for (int i = 0; i < xString.length()/2; i++) {
             if (xString.charAt(i) != xString.charAt(xString.length() - i - 1)) return false;
         }
