1f363e0d4005bb63d00687543c6d1448c1e32e2f
Tony <87371627+TonyKim9401@users.noreply.github.com>
2024-06-14 15:26:14 +0900

    Time: 1 ms (76.38%), Space: 47.9 MB (77.13%) - LeetHub

--- 2415-reverse-odd-levels-of-binary-tree/2415-reverse-odd-levels-of-binary-tree.java
+++ 2415-reverse-odd-levels-of-binary-tree/2415-reverse-odd-levels-of-binary-tree.java
@@ -15,21 +15,21 @@
  */
 class Solution {
     public TreeNode reverseOddLevels(TreeNode root) {
-        reverseOrder(root.left, root.right, 1);
+        reverseNode(root.left, root.right, 1);
         return root;
     }
 
-    public void reverseOrder(TreeNode left, TreeNode right, int depth) {
-        if (left == null || right == null) return;
+    public void reverseNode(TreeNode left, TreeNode right, int depth) {
+        if (left == null && right == null) return;
 
-        depth += 1;
-        if (depth%2 == 0) {
+        if (depth % 2 == 1) {
             int leftValue = left.val;
             int rightValue = right.val;
             left.val = rightValue;
             right.val = leftValue;
         }
-        reverseOrder(left.left, right.right, depth);
-        reverseOrder(left.right, right.left, depth);
+        depth += 1;
+        reverseNode(left.left, right.right, depth);
+        reverseNode(left.right, right.left, depth);
     }
 }
