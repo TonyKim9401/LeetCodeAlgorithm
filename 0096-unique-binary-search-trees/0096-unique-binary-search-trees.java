8230a9c16da327cde92ac925a5d9456b645cb905
Tony <87371627+TonyKim9401@users.noreply.github.com>
2024-06-15 05:40:38 +0900

    Time: 0 ms (100%), Space: 39.9 MB (91%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
@@ -1,7 +1,7 @@
 class Solution {
     public int numTrees(int n) {
-
-        int[] dp = new int[n + 1];
+        
+        int[] dp = new int[n+1];
         dp[1] = 1;
         if (n >= 2) dp[2] = 2;
         if (n >= 3) dp[3] = 5;
@@ -10,9 +10,10 @@ class Solution {
             int sum = 0;
             int right = 1;
             int left = i - 2;
-            dp[i] += dp[i-1]*2;
+            dp[i] = dp[i-1]*2;
+
             while (left >= 1) {
-                sum += dp[left] * dp[right];
+                sum += dp[right] * dp[left];
                 right += 1;
                 left -= 1;
             }
