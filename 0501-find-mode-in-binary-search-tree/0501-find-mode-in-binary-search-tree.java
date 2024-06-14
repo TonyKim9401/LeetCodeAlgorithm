aec7fea94c8b8357aa262077f3f8f934cd8735cb
Tony <87371627+TonyKim9401@users.noreply.github.com>
2024-06-14 16:06:56 +0900

    Time: 0 ms (100%), Space: 44.6 MB (82.08%) - LeetHub

--- 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.java
+++ 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.java
@@ -13,41 +13,36 @@
  *     }
  * }
  */
-
-import java.util.*;
-
 class Solution {
-
-    private int currentValue = 0;
     private int currentCount = 0;
     private int maxCount = 0;
-    private List<Integer> maxValueList = new ArrayList<>();
-
+    private int currentValue = 0;
+    private List<Integer> valueList = new ArrayList<>();
     public int[] findMode(TreeNode root) {
-        
         checkMode(root);
-        int resultLength = maxValueList.size();
-        int[] result = new int[resultLength];
-        for (int i = 0; i < resultLength; i++) {
-            result[i] = maxValueList.get(i);
+        int listSize = valueList.size();
+        int[] result = new int[listSize];
+        for (int i = 0; i < listSize; i++) {
+            result[i] = valueList.get(i);
         }
         return result;
     }
 
     public void checkMode(TreeNode node) {
-
         if (node == null) return;
 
         checkMode(node.left);
+        
         currentCount = (node.val == currentValue) ? currentCount + 1 : 1;
         if (currentCount == maxCount) {
-            maxValueList.add(node.val);
+            valueList.add(node.val);
         } else if (currentCount > maxCount) {
             maxCount = currentCount;
-            maxValueList.clear();
-            maxValueList.add(node.val);
+            valueList.clear();
+            valueList.add(node.val);
         }
         currentValue = node.val;
+
         checkMode(node.right);
     }
 }
