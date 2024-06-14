944834cbe80bdf7b9ff434f12d56a1e2fc91427a
Tony <87371627+TonyKim9401@users.noreply.github.com>
2024-06-15 07:13:10 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0067-add-binary/0067-add-binary.java
+++ 0067-add-binary/0067-add-binary.java
@@ -5,6 +5,7 @@ class Solution {
         int j = b.length() - 1;
         int carry = 0;
         StringBuilder sb = new StringBuilder();
+
         while (i >= 0 || j >= 0) {
             int sum = carry;
             if (i >= 0) sum += a.charAt(i--) - '0';
@@ -13,6 +14,6 @@ class Solution {
             sb.append(sum%2);
         }
         if (carry != 0) sb.append(1);
-        return sb.reverse().toString();
+        return sb.reverse().tosTring();
     }
 }
