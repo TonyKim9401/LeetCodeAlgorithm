23f9588a095647410cd5fa24392590a647d6fa8b
Tony <87371627+TonyKim9401@users.noreply.github.com>
2024-06-14 14:33:01 +0900

    Time: 6 ms (58.92%), Space: 44.9 MB (29.85%) - LeetHub

--- 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.java
+++ 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.java
@@ -1,44 +1,23 @@
 class Solution {
     public int[] answerQueries(int[] nums, int[] queries) {
         
-        // queries = { 3 }, nums { 3 }
-        // if (queries.length == 1 && nums.length == 1 && queries[0] == nums[0]) {
-        //     return new int[]{ 1 };
-        // }
-
-        // 1 <= n <= 1000
-        // 1 <= m <= 1000
-        // 1 <= nums.length <= 10^6
-        // 1 <= queries.length <= 10^6 (10 power 6)
-
-        // declare int variables n, m based the given arrays named nums and queries
-
         int n = nums.length;
         int m = queries.length;
 
-        // sort nums array with ascending
-        Arrays.sort(nums);
-        // [1,2,4,5];
-        // [1,1+2,1+2+4,1+2+4+5];
-        //   1,2,3,4 .... 
-        //-> [1,3,6,12];
+        Arrays.sort(nums);        
         for (int i = 1; i < n; i++) {
-            nums[i] += nums[i-1];
+            nums[i] += nums[i - 1];
         }
-
         int[] output = new int[m];
 
-        // use 2 for-loop to figure out the output.
         for (int i = 0; i < m; i++) {
             int q = queries[i];
 
             int start = 0;
             int end = n - 1;
-
-            // declare int variables to adopt binary search
             while (start <= end) {
                 int mid = start + (end - start) / 2;
-                
+
                 if (nums[mid] == q) {
                     output[i] = mid + 1;
                     break;
@@ -50,6 +29,6 @@ class Solution {
                 }
             }
         }
-        return output;        
+        return output;
     }
 }
