db3920162906556ecbc540b5959dd537ed2770f4
Tony <87371627+TonyKim9401@users.noreply.github.com>
2024-06-16 01:59:24 +0900

    Time: 0 ms (100%), Space: 41.7 MB (36.48%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,6 +1,6 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-        for (int i = digits.length - 1; i >= 0; i--) {
+        for (int i = digits.length-1; i>=0; i--) {
             if (digits[i] < 9) {
                 digits[i] += 1;
                 return digits;
