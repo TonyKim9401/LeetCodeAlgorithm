251c826cfa26f55c646ba4422b4b8151a8583c0e
Tony <87371627+TonyKim9401@users.noreply.github.com>
2024-06-16 02:01:35 +0900

    Time: 1 ms (100%), Space: 42.1 MB (62.77%) - LeetHub

--- 0067-add-binary/0067-add-binary.java
+++ 0067-add-binary/0067-add-binary.java
@@ -5,10 +5,11 @@ class Solution {
         int j = b.length() - 1;
         int carry = 0;
         StringBuilder sb = new StringBuilder();
+
         while (i >= 0 || j >= 0) {
             int sum = carry;
-            if (i >= 0) sum += a.charAt(i--) - '0';
-            if (j >= 0) sum += b.charAt(j--) - '0';
+            if (i>=0) sum += a.charAt(i--) - '0';
+            if (j>=0) sum += b.charAt(j--) - '0';
             carry = sum < 2 ? 0 : 1;
             sb.append(sum%2);
         }
