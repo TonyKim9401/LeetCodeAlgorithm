4b18abd1552d9ac3d9dd4ed5a4a9ae6cb46ec71b
Tony <87371627+TonyKim9401@users.noreply.github.com>
2024-06-17 03:53:53 +0900

    Time: 5 ms (72.68%), Space: 43.6 MB (89.76%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -2,17 +2,12 @@ class Solution {
     public boolean isPalindrome(int x) {
         
         if (x < 0) return false;
-        if (x != 0 && x % 10 == 0) return false;
-        long[] wrongAnswer = new long[]{123, 213, 1122, 123123,
-        1000021, 10000021, 10022201, 120030221, 1234567899, 1000030001, 2147483647};
 
-        for (int i = 0; i < wrongAnswer.length; i++) {
-            if (x == wrongAnswer[i]) return false;
+        String xString = String.valueOf(x);
+        int length = xString.length();
+        for (int i = 0; i < length / 2; i++) {
+            if (xString.charAt(i) != xString.charAt(length - 1 - i)) return false;
         }
-        // if (x == 123 || x == 213 || x == 1122 || x == 123123 ||
-        // x == 1000021 || x == 10000021 || x == 10022201 || x == 120030221 ||
-        // x == 1234567899 || x == 1000030001 || x == 2147483647) return false;
-        
         return true;
     }
 }
