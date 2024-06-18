bbab2f55d72f10e35a23f13eb58468718d9d9461
Tony <87371627+TonyKim9401@users.noreply.github.com>
2024-06-19 06:34:34 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -7,14 +7,10 @@ class Solution {
         while (start <= end) {
             int mid = start + (end - start) / 2;
 
-            if (Math.pow(mid, 2) == x) {
-                return mid;
-            } else if (Math.pow(mid, 2) < x) {
-                start = mid + 1;
-            } else {
-                end = mid - 1;
-            }
+            if (Math.pow(mid, 2) == x) return mid;
+            else if (Math.pow(mid, 2) < x) start = mid + 1;
+            else end = mid - 1;
         }
-        return start - 1;
+        return start + 1;
     }
 }
