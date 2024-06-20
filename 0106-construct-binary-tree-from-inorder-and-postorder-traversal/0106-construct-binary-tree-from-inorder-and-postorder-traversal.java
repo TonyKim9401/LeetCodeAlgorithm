3941f2126e0c3b3f9659a8bf3b2a4030fc36e113
Tony <87371627+TonyKim9401@users.noreply.github.com>
2024-06-20 13:31:51 +0900

    Time: 0 ms (100%), Space: 43.8 MB (87.89%) - LeetHub

--- 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.java
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.java
@@ -14,36 +14,24 @@
  * }
  */
 class Solution {
-   public TreeNode buildTree(int[] inorder, int[] postorder) {
-        // Call the recursive function with full arrays and return the result
-        return buildTree(inorder, 0, inorder.length - 1, postorder, 0, postorder.length - 1);
+    private int i, p;
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        i = inorder.length - 1;
+        p = postorder.length - 1;
+        return builder(inorder, postorder, Integer.MIN_VALUE);
     }
-    
-    private TreeNode buildTree(int[] inorder, int inStart, int inEnd, int[] postorder, int postStart, int postEnd) {
-        // Base case
-        if (inStart > inEnd || postStart > postEnd) {
+
+    public TreeNode builder(int[] inorder, int[] postorder, int stop) {
+        if (p < 0) return null;
+        if (inorder[i] == stop) {
+            i -= 1;
             return null;
         }
-        
-        // Find the root node from the last element of postorder traversal
-        int rootVal = postorder[postEnd];
-        TreeNode root = new TreeNode(rootVal);
-        
-        // Find the index of the root node in inorder traversal
-        int rootIndex = 0;
-        for (int i = inStart; i <= inEnd; i++) {
-            if (inorder[i] == rootVal) {
-                rootIndex = i;
-                break;
-            }
-        }
-        
-        // Recursively build the left and right subtrees
-        int leftSize = rootIndex - inStart;
-        int rightSize = inEnd - rootIndex;
-        root.left = buildTree(inorder, inStart, rootIndex - 1, postorder, postStart, postStart + leftSize - 1);
-        root.right = buildTree(inorder, rootIndex + 1, inEnd, postorder, postEnd - rightSize, postEnd - 1);
-        
-        return root;
+
+        TreeNode output = new TreeNode(postorder[p]);
+        p -= 1;
+        output.right = builder(inorder, postorder, output.val);
+        output.left = builder(inorder, postorder, stop);
+        return output;
     }
 }
