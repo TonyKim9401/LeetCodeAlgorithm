5f033e525d03af24db46aee5b10bbe7cf7e15b11
Tony <87371627+TonyKim9401@users.noreply.github.com>
2024-06-27 13:25:03 +0900

    Time: 5 ms (33.81%), Space: 46.1 MB (5.68%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,17 +1,19 @@
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
         
+        int output = 0;
         for (int i = triangle.size() - 1; i > 0; i--) {
             List<Integer> upRow = triangle.get(i-1);
             List<Integer> downRow = triangle.get(i);
-
+            List<Integer> newUpRow = new ArrayList<>();
             for (int j = 0; j < upRow.size(); j++) {
-                int upNumber = upRow.get(j);
-                int downRowLeft = downRow.get(j);
-                int downRowRight = downRow.get(j+1);
-                upNumber = Math.min(upNumber + downRowLeft, upNumber + downRowRight);
-                upRow.set(j, upNumber);
+                int up = upRow.get(j);
+                int a = downRow.get(j);
+                int b = downRow.get(j+1);
+                up = Math.min(up + a, up + b);
+                newUpRow.add(up);
             }
+            triangle.set(i-1, newUpRow);
         }
         return triangle.get(0).get(0);
     }
