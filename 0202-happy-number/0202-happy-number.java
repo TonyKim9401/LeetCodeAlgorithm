4eccc75df359398a621aff0fdbe789d8a08db6aa
Tony <87371627+TonyKim9401@users.noreply.github.com>
2024-07-07 13:40:03 +0900

    Time: 2 ms (20.22%), Space: 41.3 MB (5.63%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isHappy(int n) {
+        if (n == 1) return true;
+
+        int happy = n;
+        while (happy > 0) {
+            String num = String.valueOf(happy);
+            happy = 0;
+            for (int i = 0; i < num.length(); i++) {
+                happy += Math.pow(Integer.valueOf(String.valueOf(num.charAt(i))), 2);
+            }
+
+            if (happy == 1) return true;
+            if (happy < 5) return false;
+        }
+        return false;
+    }
+}
