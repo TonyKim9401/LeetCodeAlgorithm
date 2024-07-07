703820e52cce16bd628ee79c7467ceec77ba516b
Tony <87371627+TonyKim9401@users.noreply.github.com>
2024-07-07 14:09:08 +0900

    Time: 0 ms (100%), Space: 40.8 MB (22.48%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -1,27 +1,25 @@
 class Solution {
     public boolean isHappy(int n) {
-
+        
         int happy = getSquare(n);
-        while (!alwaysHappy(happy) && !alwaysUnhappy(happy)) {
+        while (happy != 1 && happy != 7 && happy != 4) {
             happy = getSquare(happy);
         }
-        return alwaysHappy(happy);
+        return isHappyCheck(happy);
+    }
+
+    public boolean isHappyCheck(int n) {
+        if (n == 1 || n == 7) return true;
+        return false;
     }
 
     public int getSquare(int n) {
         int result = 0;
         while (n > 0) {
-            int value = n % 10;
-            result += Math.pow(value,2);
+            int rest = n % 10;
+            result += Math.pow(rest, 2);
             n /= 10;
         }
         return result;
-    }
-
-    public boolean alwaysHappy(int n) {
-        return n == 1 || n == 7;
-    }
-    public boolean alwaysUnhappy(int n) {
-        return n == 4;
     }
 }
