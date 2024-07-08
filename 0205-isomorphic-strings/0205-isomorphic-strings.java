feee21e6e2bc53af3b29af4ecfd8b1d508be536b
Tony <87371627+TonyKim9401@users.noreply.github.com>
2024-07-09 04:48:33 +0900

    Time: 7 ms (84.6%), Space: 42.4 MB (42.32%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -3,11 +3,8 @@ class Solution {
         int[] sArray = new int[128];
         int[] tArray = new int[128];
 
-        if (s.length() != t.length()) return false;
-
         for (int i = 0; i < s.length(); i++) {
             if (sArray[s.charAt(i)] != tArray[t.charAt(i)]) return false;
-
             sArray[s.charAt(i)] = i + 1;
             tArray[t.charAt(i)] = i + 1;
         }
