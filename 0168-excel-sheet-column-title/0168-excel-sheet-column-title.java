42ef90fc5c43505e31f25c0da05764e3d7426905
Tony <87371627+TonyKim9401@users.noreply.github.com>
2024-07-10 08:40:08 +0900

    Time: 0 ms (100%), Space: 40.4 MB (87.49%) - LeetHub

--- 0168-excel-sheet-column-title/0168-excel-sheet-column-title.java
+++ 0168-excel-sheet-column-title/0168-excel-sheet-column-title.java
@@ -1,22 +1,11 @@
 class Solution {
     public String convertToTitle(int columnNumber) {
-        Map<Integer, String> map = new HashMap<>();
-        for (int i = 1; i < 27; i++) {
-            map.put(i, String.valueOf((char)('A'+i-1)));
-        }
         StringBuilder sb = new StringBuilder();
-
-        while (columnNumber > 26) {
-            int key = columnNumber % 26;
-            if (key == 0) {
-                key = 26;
-                columnNumber -= 1;
-            }
-            sb.append(map.get(key));
+        while (columnNumber > 0) {
+            columnNumber -= 1;
+            char c = (char) ('A' + columnNumber % 26);
+            sb.append(c);
             columnNumber /= 26;
-        }
-        if (columnNumber <= 26) {
-            sb.append(map.get(columnNumber));
         }
         return sb.reverse().toString();
     }
