36024ff63fad7858e105326f7c980a441c3b3c4b
Tony <87371627+TonyKim9401@users.noreply.github.com>
2024-07-12 10:52:25 +0900

    Time: 4 ms (41.19%), Space: 45 MB (32.49%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.java
+++ 0047-permutations-ii/0047-permutations-ii.java
@@ -2,24 +2,24 @@ class Solution {
     public List<List<Integer>> permuteUnique(int[] nums) {
         Arrays.sort(nums);
         List<List<Integer>> output = new ArrayList<>();
-        backtracking(nums, new ArrayList<>(), new ArrayList<>(),output);
+        backtracking(nums, new ArrayList<>(), new ArrayList<>(), output);
         return output;
     }
 
     public void backtracking(int[] nums, List<Integer> inside, List<Integer> check, List<List<Integer>> output) {
-        if (inside.size() == nums.length) {
+        if (nums.length == inside.size()) {
             output.add(new ArrayList<>(inside));
             return;
         }
 
         for (int i = 0; i < nums.length; i++) {
-            if (!check.contains(i)){
+            if (!check.contains(i)) {
                 inside.add(nums[i]);
                 check.add(i);
                 backtracking(nums, inside, check, output);
                 check.remove(check.size()-1);
                 inside.remove(inside.size()-1);
-                while (i < nums.length-1 && nums[i] == nums[i+1]) i += 1;
+                while (i < nums.length -1 && nums[i] == nums[i+1]) i += 1;
             }
         }
     }
