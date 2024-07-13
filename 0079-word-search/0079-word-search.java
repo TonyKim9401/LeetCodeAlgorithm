de489647863efd39edbea7c747392d4fe96c1190
Tony <87371627+TonyKim9401@users.noreply.github.com>
2024-07-13 09:37:16 +0900

    Time: 142 ms (62.2%), Space: 41.7 MB (32.57%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,27 +1,27 @@
 class Solution {
     public boolean exist(char[][] board, String word) {
+        boolean[][] path = new boolean[board.length][board[0].length];
+
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[0].length; j++) {
-                if (board[i][j] == word.charAt(0)) {
-                    boolean[][] path = new boolean[board.length][board[0].length];
-                    if (wordCheck(i, j, 0, board, word, path)) return true;
-                }
+                if (check(i, j, 0, board, word, path)) return true;
             }
         }
         return false;
     }
 
-    public boolean wordCheck(int i, int j, int idx, char[][] board, String word, boolean[][] path) {
-        if (i < 0 || i > board.length - 1 || j < 0 || j > board[0].length - 1) return false;
-        if (path[i][j]) return false;
+    public boolean check(int i, int j, int idx, char[][] board, String word, boolean[][] path) {
+        if (idx == word.length()) return true;
+        if (i < 0 || i > board.length-1 || j < 0 || j > board[0].length-1) return false;
         if (board[i][j] != word.charAt(idx)) return false;
-        if (idx == word.length()-1) return true;
+        if (path[i][j]) return false;
         idx += 1;
         path[i][j] = true;
-        if (wordCheck(i+1, j, idx, board, word, path)||
-            wordCheck(i-1, j, idx, board, word, path)||
-            wordCheck(i, j+1, idx, board, word, path)||
-            wordCheck(i, j-1, idx, board, word, path)) return true;
+        if (check(i+1, j, idx, board, word, path) ||
+            check(i-1, j, idx, board, word, path) ||
+            check(i, j+1, idx, board, word, path) ||
+            check(i, j-1, idx, board, word, path)
+        ) return true;
         path[i][j] = false;
         return false;
     }
