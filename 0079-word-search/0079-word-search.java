72f7a305aac7265243a01fec28240c8b5251e17c
Tony <87371627+TonyKim9401@users.noreply.github.com>
2024-07-14 05:13:57 +0900

    Time: 141 ms (63.85%), Space: 41.2 MB (80.59%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,28 +1,29 @@
 class Solution {
     public boolean exist(char[][] board, String word) {
-        boolean[][] path = new boolean[board.length][board[0].length];
+        boolean[][] check;
 
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[0].length; j++) {
-                if (check(i, j, 0, board, word, path)) return true;
+                check = new boolean[board.length][board[0].length];
+                if(direction(i, j, 0, board, word, check)) return true;
             }
         }
         return false;
     }
 
-    public boolean check(int i, int j, int idx, char[][] board, String word, boolean[][] path) {
-        if (idx == word.length()) return true;
-        if (i < 0 || i > board.length-1 || j < 0 || j > board[0].length-1) return false;
+    public boolean direction(int i, int j, int idx, char[][] board, String word, boolean[][] check) {
+        if (word.length() == idx) return true;
+        if (i < 0 || i > board.length-1 || j < 0 || j > board[0].length -1) return false;
         if (board[i][j] != word.charAt(idx)) return false;
-        if (path[i][j]) return false;
+        if (check[i][j]) return false;
         idx += 1;
-        path[i][j] = true;
-        if (check(i+1, j, idx, board, word, path) ||
-            check(i-1, j, idx, board, word, path) ||
-            check(i, j+1, idx, board, word, path) ||
-            check(i, j-1, idx, board, word, path)
+        check[i][j] = true;
+        if (direction(i+1, j, idx, board, word, check) ||
+            direction(i-1, j, idx, board, word, check) ||
+            direction(i, j+1, idx, board, word, check) ||
+            direction(i, j-1, idx, board, word, check)
         ) return true;
-        path[i][j] = false;
+        check[i][j] = false;
         return false;
     }
 }
