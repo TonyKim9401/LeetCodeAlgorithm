420e3bc13aac11e48fec72d637a2dcaba9a8fa0b
Tony <87371627+TonyKim9401@users.noreply.github.com>
2024-07-15 07:01:45 +0900

    Time: 143 ms (60.6%), Space: 41.6 MB (40.47%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -5,23 +5,24 @@ class Solution {
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[0].length; j++) {
                 check = new boolean[board.length][board[0].length];
-                if(direction(i, j, 0, board, word, check)) return true;
+                if (wordCheck(i, j, 0, board, word, check)) return true;
             }
         }
         return false;
     }
 
-    public boolean direction(int i, int j, int idx, char[][] board, String word, boolean[][] check) {
-        if (word.length() == idx) return true;
-        if (i < 0 || i > board.length-1 || j < 0 || j > board[0].length -1) return false;
+    public boolean wordCheck(int i, int j, int idx, char[][] board, String word, boolean[][] check) {
+        if (idx == word.length()) return true;
+        if (i < 0 || i > board.length - 1 || j < 0 || j > board[0].length - 1) return false;
         if (board[i][j] != word.charAt(idx)) return false;
         if (check[i][j]) return false;
         idx += 1;
         check[i][j] = true;
-        if (direction(i+1, j, idx, board, word, check) ||
-            direction(i-1, j, idx, board, word, check) ||
-            direction(i, j+1, idx, board, word, check) ||
-            direction(i, j-1, idx, board, word, check)
+        if (
+            wordCheck(i+1, j, idx, board, word, check) ||
+            wordCheck(i-1, j, idx, board, word, check) ||
+            wordCheck(i, j+1, idx, board, word, check) ||
+            wordCheck(i, j-1, idx, board, word, check)
         ) return true;
         check[i][j] = false;
         return false;
