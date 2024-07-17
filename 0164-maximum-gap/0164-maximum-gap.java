5b2b12712c339eadf0577fd188e428ecfc5b6939
Tony <87371627+TonyKim9401@users.noreply.github.com>
2024-07-17 15:29:14 +0900

    Time: 40 ms (62.46%), Space: 60.6 MB (62.46%) - LeetHub

--- 0164-maximum-gap/0164-maximum-gap.java
+++ 0164-maximum-gap/0164-maximum-gap.java
@@ -3,7 +3,8 @@ class Solution {
         Arrays.sort(nums);
         int output = 0;
         for (int i = 0; i < nums.length-1; i++) {
-            output = output > nums[i+1] - nums[i] ? output : nums[i+1] - nums[i];
+            int gap = nums[i+1] - nums[i];
+            output = output > gap ? output : gap;
         }
         return output;
     }
