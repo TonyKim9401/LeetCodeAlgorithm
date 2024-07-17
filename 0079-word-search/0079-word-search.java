0ab8537193f1b3fa2fa80c48a5077c0209889565
Tony <87371627+TonyKim9401@users.noreply.github.com>
2024-07-17 15:19:54 +0900

    Time: 143 ms (60.68%), Space: 41.4 MB (68.01%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,26 +1,28 @@
 class Solution {
     public boolean exist(char[][] board, String word) {
         boolean[][] check = new boolean[board.length][board[0].length];
+
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[0].length; j++) {
-                if (wordCheck(i, j, 0, board, word, check)) return true;
+                if (boardCheck(i, j, 0, board, word, check)) return true;
             }
         }
         return false;
     }
 
-    public boolean wordCheck(int i, int j, int idx, char[][] board, String word, boolean[][] check) {
+    public boolean boardCheck(int i, int j, int idx, char[][] board, String word, boolean[][] check) {
         if (idx == word.length()) return true;
-        if (i < 0 || i > board.length-1 || j < 0 || j > board[0].length-1) return false;
-        if (check[i][j]) return false;
-        if (board[i][j] != word.charAt(idx)) return false;
+        if (
+            i < 0 || i > board.length-1 || j < 0 || j > board[0].length-1 ||
+            check[i][j] || board[i][j] != word.charAt(idx)
+        ) return false;
         idx += 1;
         check[i][j] = true;
         if (
-            wordCheck(i+1, j, idx, board, word, check) ||
-            wordCheck(i-1, j, idx, board, word, check) ||
-            wordCheck(i, j+1, idx, board, word, check) ||
-            wordCheck(i, j-1, idx, board, word, check)
+            boardCheck(i+1, j, idx, board, word, check) ||
+            boardCheck(i-1, j, idx, board, word, check) ||
+            boardCheck(i, j+1, idx, board, word, check) ||
+            boardCheck(i, j-1, idx, board, word, check)
         ) return true;
         check[i][j] = false;
         return false;
