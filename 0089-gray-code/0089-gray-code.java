18aa8210766b998df460002aac849bddd3638af3
Tony <87371627+TonyKim9401@users.noreply.github.com>
2024-07-19 08:03:53 +0900

    Time: 7 ms (65.43%), Space: 51.4 MB (47.29%) - LeetHub

---
+++ 0089-gray-code/0089-gray-code.java
@@ -0,0 +1,14 @@
+class Solution {
+    public List<Integer> grayCode(int n) {
+        List<Integer> output = new ArrayList<>();
+        output.add(0);
+        for (int i = 0; i < n; i++) {
+            int size = output.size();
+            for (int j = size-1; j >= 0; j--) {
+                int inside = output.get(j) + (1 << i);
+                output.add(inside);
+            }
+        }
+        return output;
+    }
+}
