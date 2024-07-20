1eb2e7256991c6713a4f310d767b8c252f7f0eec
Tony <87371627+TonyKim9401@users.noreply.github.com>
2024-07-20 10:06:48 +0900

    Time: 2 ms (86.22%), Space: 45.4 MB (31.9%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -1,25 +1,50 @@
 class Solution {
     public void solve(char[][] board) {
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[0].length; j++) {
-                if (board[i][j] == 'O') convert(i, j, board);
-            } 
+        if( board.length < 2 || board[0].length < 2){
+            return;
         }
+        
+        for(int i = 0; i < board[0].length; i++){
+            if(board[0][i] == 'O'){
+                dfs(board, 0, i);
+            }
+            if(board[board.length-1][i] == 'O'){
+                dfs(board, board.length-1, i);
+            }
+        }
+        
+        for(int i = 0; i < board.length; i++){
+            if(board[i][0] == 'O'){
+                dfs(board, i , 0);
+            }
+            if(board[i][board[0].length-1] == 'O'){
+                dfs(board, i, board[0].length-1);
+            }
+        }
+        
+        
+        for(int i = 0; i < board.length; i++){
+            for(int j = 0; j < board[0].length; j++){
+                if(board[i][j] == 'O'){
+                    board[i][j] = 'X';
+                }
+                if(board[i][j] == '*'){
+                    board[i][j] = 'O';
+                }                
+            }
+        }        
     }
-
-    public void convert(int i, int j, char[][] board) {
-        if (i < 1 || i > board.length - 2 || j < 1 || j > board[0].length-2) return;
-        if (board[i][j] == 'X') return;
-        if (
-            (board[i+1][j] == 'O' && (i+1 == board.length - 1)) ||
-            (board[i-1][j] == 'O' && (i-1 == 0)) ||
-            (board[i][j+1] == 'O' && (j+1 == board[0].length - 1)) ||
-            (board[i][j-1] == 'O' && (j-1 == 0))
-        ) return;
-        board[i][j] = 'X';
-        convert(i+1, j, board);
-        convert(i-1, j, board);
-        convert(i, j+1, board);
-        convert(i, j-1, board);
+    
+    public void dfs(char[][] board, int i, int j){
+        if(i < 0 || j < 0 || i >= board.length || j >= board[0].length || board[i][j] != 'O'){
+            return;
+        }
+        
+        board[i][j] = '*';
+        
+        dfs(board, i, j+1);
+        dfs(board, i, j-1);
+        dfs(board, i+1, j);
+        dfs(board, i-1, j);
     }
 }
