084dd6697052639cc94de6ef9a81a45db29fcce8
Tony <87371627+TonyKim9401@users.noreply.github.com>
2024-07-27 07:06:23 +0900

    Time: 0 ms (100%), Space: 43.4 MB (54.39%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -22,8 +22,7 @@ class Solution {
         if (node == null) return true;
 
         if (node.val <= min || node.val >= max) return false;
-
-        return isValid(node.left, min, node.val)
-                && isValid(node.right, node.val, max);
+        return isValid(node.left, min, node.val) &&
+                isValid(node.right, node.val, max);
     }
 }
