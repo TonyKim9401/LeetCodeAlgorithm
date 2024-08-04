eae0fa58caf581bd97d0bc295f5f00d381fce1e2
Tony <87371627+TonyKim9401@users.noreply.github.com>
2024-08-04 12:08:27 +0900

    Time: 1 ms (98.89%), Space: 41.3 MB (62.16%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -3,14 +3,12 @@ class Solution {
         Stack<Character> stack = new Stack<>();
 
         for (char c : s.toCharArray()) {
-            if (c == '(') {
-                stack.push(')');
-            } else if (c == '[') {
-                stack.push(']');
-            } else if (c == '{') {
-                stack.push('}');
-            } else if (stack.isEmpty() || stack.pop() != c) return false;
+            if (c == '(') stack.push(')');
+            else if (c == '[') stack.push(']');
+            else if (c == '{') stack.push('}');
+            else if (stack.isEmpty() || stack.pop() != c) return false;
         }
+
         return stack.isEmpty();
     }
 }
