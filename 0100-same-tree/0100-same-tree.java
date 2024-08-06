b3ac7350ab7b80cdfb16d7830ba046f5be9752f8
Tony <87371627+TonyKim9401@users.noreply.github.com>
2024-08-07 06:25:30 +0900

    Time: 0 ms (100%), Space: 40.8 MB (63.38%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -18,10 +18,11 @@ class Solution {
         if (p == null || q == null) {
             if (p == null && q == null) return true;
             return false;
-        };
+        }
 
         if (p.val != q.val) return false;
-        return isSameTree(p.left, q.left) &&
-                isSameTree(p.right, q.right);
+        
+        return isSameTree(p.left, q.left)
+            && isSameTree(p.right,q.right);
     }
 }
