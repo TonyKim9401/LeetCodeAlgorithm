d50487fd34d803c0a84a901ffd32124e043fe931
Tony <87371627+TonyKim9401@users.noreply.github.com>
2024-08-06 10:27:50 +0900

    Time: 7 ms (54.46%), Space: 46.1 MB (8.74%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,18 +1,39 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int[] output = new int[2];
+        // O(n^2)
+        // [2,7,11,15]
+        // target - element = 7;
+        // 2 : [1, 5]
+        // 7 : 2
+        // 11 : 3
+        // 15 : 4
+
+        // put all elements into the map
+        Map<Integer, List<Integer>> map = new HashMap<>();
 
         for (int i = 0; i < nums.length; i++) {
-            int first = nums[i];
-
-            for (int j = i+1; j < nums.length; j++) {
-                if (first + nums[j] == target) {
-                    output[0] = i;
-                    output[1] = j;
-                }
+            if (map.containsKey(nums[i])) {
+                List<Integer> inside = map.get(nums[i]);
+                inside.add(i);
+                map.put(nums[i], inside);
+            } else {
+                List<Integer> inside = new ArrayList<>();
+                inside.add(i);
+                map.put(nums[i],inside);
             }
         }
 
-        return output;
+        // [2,2]
+        for (int i = 0; i < nums.length; i++) {
+            int second = target - nums[i];
+            if (second == nums[i] && map.get(nums[i]).size() > 1) {
+                List<Integer> inside = map.get(nums[i]);
+                return new int[]{inside.get(0), inside.get(1)};
+            } else if (map.containsKey(second) && second != nums[i]){
+                List<Integer> inside = map.get(second);
+                return new int[]{i, inside.get(0)};
+            }
+        }
+        return new int[]{-1, -1};
     }
 }
