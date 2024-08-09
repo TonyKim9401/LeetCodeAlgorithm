fe1c850cb09424dbde606a07442aa7e754c72686
Tony <87371627+TonyKim9401@users.noreply.github.com>
2024-08-09 10:05:45 +0900

    Time: 17 ms (17.1%), Space: 48.4 MB (58.31%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,31 +1,30 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        // key: array elements, value: how often it is showed in the given array
+        
+        // nums = [1,1,1,2,2,3], k = 2
+        // 1, 3
+        // 2, 2
+        // 3, 1
+        
+        // O(nlogn)
+        // Java internally uses mergesort, quicksort(PriorityQueue)
         Map<Integer, Integer> map = new HashMap<>();
         for (int num : nums) {
-            // the num has already been found
             if (map.containsKey(num)) {
                 int count = map.get(num);
                 map.put(num, count+1);
-            // have never found the key value
             } else {
                 map.put(num, 1);
             }
         }
 
-        // sort hashmap by using its value
-        List<Map.Entry<Integer, Integer>> list = new ArrayList<>(map.entrySet());
-        Collections.sort(list, (o1, o2) -> {
-            if (o1.getValue() == o2.getValue()) {
-                return o2.getKey() - o1.getKey();
-            } else {
-                return o2.getValue() - o1.getValue();
-            }
-        }); // descending
+        // key values from the map
+        List<Integer> keyList = new ArrayList<>(map.keySet());
+        Collections.sort(keyList, (o1, o2) -> map.get(o2).compareTo(map.get(o1)));
 
         int[] output = new int[k];
         for (int i = 0; i < k; i++) {
-            output[i] = list.get(i).getKey();
+            output[i] = keyList.get(i);
         }
 
         return output;
