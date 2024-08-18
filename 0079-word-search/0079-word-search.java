54c60d600bd8e245abacaec595165150fd26eebe
Tony <87371627+TonyKim9401@users.noreply.github.com>
2024-08-19 07:59:25 +0900

    Time: 142 ms (61.7%), Space: 41.3 MB (86.92%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,5 +1,13 @@
 class Solution {
     public boolean exist(char[][] board, String word) {
+        // Time complexity: O(n * m * 4^k);
+        // -> The size of board: n * m
+        // -> Check 4 directions by the given word's length: 4^k
+        // Space complexity: O(n * m + k)
+        // -> boolean 2D array: n * M
+        // -> recursive max k spaces
+
+        // Mark visited path to do not go back.
         boolean[][] visit;
 
         for (int i = 0; i < board.length; i++) {
@@ -12,22 +20,34 @@ class Solution {
     }
 
     private boolean wordSearch(int i, int j, int idx, String word, char[][] board, boolean[][] visit) {
+
+        // When idx checking reach to the end of the length of the word then, return true
         if (idx == word.length()) return true;
+
+        // Check if i and j are inside of the range
         if (i < 0 || i >= board.length || j < 0 || j >= board[0].length) return false;
+
+        // Check if the coordinate equals to the charactor value
         if (board[i][j] != word.charAt(idx)) return false;
         if (visit[i][j]) return false;
 
+        // Mark the coordinate as visited
         visit[i][j] = true;
+
+        // If visited, the target is gonna be the next charactor
         idx += 1;
+
+        // if any direction returns true then it is true
         if (
             wordSearch(i+1, j, idx, word, board, visit) ||
             wordSearch(i-1, j, idx, word, board, visit) ||
             wordSearch(i, j+1, idx, word, board, visit) ||
             wordSearch(i, j-1, idx, word, board, visit)
         ) return true;
+
+        // if 
         visit[i][j] = false;
 
         return false;
-
     }
 }
