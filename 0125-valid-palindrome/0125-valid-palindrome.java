0904df4e3121abe3743b3b8a253bd99dff1fa719
Tony <87371627+TonyKim9401@users.noreply.github.com>
2024-08-19 03:44:40 +0900

    Time: 2 ms (99.12%), Space: 43 MB (63.88%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,34 +1,35 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        
-        int i = 0;
-        int j = s.length() - 1;
-
-        while (i <= j) {
-            int start = s.charAt(i);
-            int end = s.charAt(j);
-            if (!isLetter(start)) {
-                i += 1;
-                continue;
-            }
-            if (!isLetter(end)) {
-                j -= 1;
-                continue;
-            }
-
-            start = start >= 'a' ? start - 'a' +'A' : start;
-            end = end >= 'a' ? end - 'a' + 'A' : end;
-            if (start != end) return false;
-            i += 1;
-            j -= 1;
-        }
-        return true;
+        String target = checkString(s);
+        return checkPalindrome(target);
     }
 
-    public boolean isLetter(int c) {
-        if (c >= 'a' && c <= 'z') return true;
-        if (c >= 'A' && c <= 'Z') return true;
-        if (c >= '0' && c <= '9') return true;
-        return false;
+    private String checkString(String s) {
+        StringBuilder sb = new StringBuilder();
+        for (char c : s.toCharArray()) {
+            if (c >= 'a' && c <= 'z') {
+                sb.append(c);
+            }
+            if (c >= 'A' && c <= 'Z') {
+                c = (char)(c - 'A' + 'a');
+                sb.append(c);
+            }
+            if (c >= '0' && c <= '9') {
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+
+    private Boolean checkPalindrome(String target) {
+        int start = 0;
+        int end = target.length() - 1;
+
+        while (start <= end) {
+            if (target.charAt(start) != target.charAt(end)) return false;
+            start += 1;
+            end -= 1;
+        }
+        return true;
     }
 }
