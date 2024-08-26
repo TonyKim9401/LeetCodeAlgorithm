4f0b28dd54995023c83047c6bd6fb007b922cb95
Tony <87371627+TonyKim9401@users.noreply.github.com>
2024-08-27 05:15:32 +0900

    Time: 52 ms (39.08%), Space: 57.9 MB (72.01%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -9,21 +9,17 @@ class Solution {
         int maxLength = 0;
 
         for (int num : nums) {
-            // 이미 처리된 숫자는 무시
             if (map.containsKey(num)) continue;
 
             int left = map.getOrDefault(num - 1, 0);
             int right = map.getOrDefault(num + 1, 0);
             int currentLength = left + 1 + right;
 
-            // 현재 수열의 길이 갱신
             map.put(num, currentLength);
 
-            // 양 끝 수열도 갱신
             map.put(num - left, currentLength);
             map.put(num + right, currentLength);
 
-            // 최대 수열 길이 갱신
             maxLength = Math.max(maxLength, currentLength);
         }
 
