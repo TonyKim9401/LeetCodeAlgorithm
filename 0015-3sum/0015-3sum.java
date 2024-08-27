65b3f7e5bfa67d220b56128a9eef9587bc03bc88
Tony <87371627+TonyKim9401@users.noreply.github.com>
2024-08-28 08:00:04 +0900

    Time: 228 ms (29.12%), Space: 52 MB (37.18%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,29 +1,38 @@
-class Solution {
+public class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        Arrays.sort(nums);
-        List<List<Integer>> output = new ArrayList<>();
-
-        for (int i = 0; i < nums.length; i++) {
-            if (i > 0 && nums[i] == nums[i-1]) continue;
-
-            int k = nums.length - 1;
-
-            int j = i + 1;
-            while (j < k) {
+        Arrays.sort(nums); // Sorted Array
+        List<List<Integer>> answer = new ArrayList<>();
         
-                int sum = nums[i]+nums[j]+nums[k];
-                
-                if (sum > 0) k -= 1;
-                else if (sum < 0) j += 1;
-                else {
-                    output.add(List.of(nums[i], nums[j], nums[k]));
-                    j += 1;
-                    while (j < k && nums[j] == nums[j-1]) {
-                        j += 1;
-                    }
-                }
-            }
+        if (nums.length < 3) {
+            return answer;
         }
-        return output;
+        
+        if (nums[0] > 0) {
+            return answer;
+        }
+        
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+        
+        for (int i = 0; i < nums.length; ++i) {
+            hashMap.put(nums[i], i);
+        }
+        
+        for (int i = 0; i < nums.length - 2; ++i) {
+            if (nums[i] > 0) {
+                break;
+            }
+            
+            for (int j = i + 1; j < nums.length - 1; ++j) {
+                int required = -1 * (nums[i] + nums[j]);
+                if (hashMap.containsKey(required) && hashMap.get(required) > j) {
+                    answer.add(Arrays.asList(nums[i], nums[j], required));
+                }
+                j = hashMap.get(nums[j]);
+            }
+            
+            i = hashMap.get(nums[i]);
+        }
+        
+        return answer;
     }
 }
