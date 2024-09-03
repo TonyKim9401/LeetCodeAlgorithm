cf5f8b75db9bd734f74a0aadf98aac85e675edea
Tony <87371627+TonyKim9401@users.noreply.github.com>
2024-09-03 09:28:14 +0900

    Time: 227 ms (28.92%), Space: 51.6 MB (48.11%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,38 +1,28 @@
 public class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
+
         Arrays.sort(nums); // Sorted Array
-        List<List<Integer>> answer = new ArrayList<>();
+
+        List<List<Integer>> output = new ArrayList<>();
+        Map<Integer, Integer> map = new HashMap<>();
         
-        if (nums.length < 3) {
-            return answer;
-        }
-        
-        if (nums[0] > 0) {
-            return answer;
-        }
-        
-        HashMap<Integer, Integer> hashMap = new HashMap<>();
-        
-        for (int i = 0; i < nums.length; ++i) {
-            hashMap.put(nums[i], i);
-        }
+        for (int i = 0; i < nums.length; ++i) map.put(nums[i], i);
         
         for (int i = 0; i < nums.length - 2; ++i) {
-            if (nums[i] > 0) {
-                break;
-            }
+            if (nums[i] > 0) break;
             
             for (int j = i + 1; j < nums.length - 1; ++j) {
-                int required = -1 * (nums[i] + nums[j]);
-                if (hashMap.containsKey(required) && hashMap.get(required) > j) {
-                    answer.add(Arrays.asList(nums[i], nums[j], required));
+                int cValue = -1 * (nums[i] + nums[j]);
+
+                if (map.containsKey(cValue) && map.get(cValue) > j) {
+                    output.add(List.of(nums[i], nums[j], cValue));
                 }
-                j = hashMap.get(nums[j]);
+                j = map.get(nums[j]);
             }
             
-            i = hashMap.get(nums[i]);
+            i = map.get(nums[i]);
         }
         
-        return answer;
+        return output;
     }
 }
