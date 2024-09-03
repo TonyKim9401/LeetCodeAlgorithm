444a5fa4f234fa8f41f9f93541fe6ddafa26866d
Tony <87371627+TonyKim9401@users.noreply.github.com>
2024-09-03 09:52:34 +0900

    Time: 1 ms (98.7%), Space: 41.4 MB (53.92%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,8 +1,9 @@
 class Solution {
     public boolean isValid(String s) {
+
         Stack<Character> stack = new Stack<>();
 
-        for (char c : s.toCharArray()) {
+        for(char c : s.toCharArray()) {
             if (c == '(') {
                 stack.push(')');
             } else if (c == '[') {
@@ -11,6 +12,7 @@ class Solution {
                 stack.push('}');
             } else if (stack.isEmpty() || stack.pop() != c) return false;
         }
+        
         return stack.isEmpty();
     }
 }
