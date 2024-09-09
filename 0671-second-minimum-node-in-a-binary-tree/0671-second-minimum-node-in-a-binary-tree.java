315cbb75ec3aecbd6ffbabbcfb9ba9e420cbd091
Tony <87371627+TonyKim9401@users.noreply.github.com>
2024-09-10 02:42:57 +0900

    Time: 0 ms (100%), Space: 40.6 MB (83.23%) - LeetHub

---
+++ 0671-second-minimum-node-in-a-binary-tree/0671-second-minimum-node-in-a-binary-tree.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int findSecondMinimumValue(TreeNode root) {
+         TreeSet<Integer> s = new TreeSet<>();
+         solve(root, s);
+
+         s.pollFirst();
+         if (!s.isEmpty()) return s.first();
+         return -1;
+    }
+
+    public void solve(TreeNode node, Set<Integer> s) {
+        if (node != null) {
+            s.add(node.val);
+            solve(node.left, s);
+            solve(node.right, s);
+        }
+    }
+}
