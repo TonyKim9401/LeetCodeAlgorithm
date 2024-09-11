7caca0b6c72a9995d50d45dc6d38a6212cb0254e
Tony <87371627+TonyKim9401@users.noreply.github.com>
2024-09-12 07:59:18 +0900

    Time: 1 ms (38.54%), Space: 42.1 MB (39.18%) - LeetHub

--- 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.java
+++ 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.java
@@ -1,43 +1,36 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- * int val;
- * TreeNode left;
- * TreeNode right;
- * TreeNode() {}
- * TreeNode(int val) { this.val = val; }
- * TreeNode(int val, TreeNode left, TreeNode right) {
- * this.val = val;
- * this.left = left;
- * this.right = right;
- * }
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
 class Solution {
-    int sum = 0;
-
-    private void dfs(TreeNode root, StringBuilder sb) {
-        if (root == null)
-            return;
-        sb.append(root.val);
-        if (root.left == null && root.right == null) {
-            int sm = help(sb);
-            sum += sm;
-        }
-        dfs(root.left, sb);
-        dfs(root.right, sb);
-        sb.deleteCharAt(sb.length() - 1);
-    }
-
-    private int help(StringBuilder sb) {
-        return Integer.parseInt(sb.toString(), 2);
-    }
-
+    private int sum = 0;
     public int sumRootToLeaf(TreeNode root) {
-        if (root == null)
-            return 0;
-        StringBuilder sb = new StringBuilder("");
-        dfs(root, sb);
+        StringBuilder sb = new StringBuilder();
+        backtracking(root, sb);
         return sum;
+    }
+
+    public void backtracking(TreeNode node, StringBuilder sb) {
+        if (node == null) return;
+        sb.append(String.valueOf(node.val));
+
+        if (node.left == null && node.right == null) {
+            sum += Integer.valueOf(sb.toString(), 2);
+        } else {
+            backtracking(node.left, sb);
+            backtracking(node.right, sb);
+        }
+        sb.deleteCharAt(sb.length() - 1);
     }
 }
