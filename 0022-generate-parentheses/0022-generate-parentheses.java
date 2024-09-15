d65c6bca2b1f5ca3be700db45f2688480e7cfe0e
Tony <87371627+TonyKim9401@users.noreply.github.com>
2024-09-15 13:03:31 +0900

    Time: 0 ms (100%), Space: 42.9 MB (80.59%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,22 +1,37 @@
 class Solution {
+    private List<String> output = new ArrayList<>();
+    private StringBuilder sb = new StringBuilder();
+    private int[] counts;
     public List<String> generateParenthesis(int n) {
-        List<String> output = new ArrayList<>();
-        backtracking(n, 0, 0, "", output);
+        counts = new int[2];
+        counts[0] = n;
+        counts[1] = n;
+        backtracking();
         return output;
     }
 
-    public void backtracking(int n, int open, int close, String str, List<String> output) {
-        if (open == close && open == n) {
-            output.add(str);
+    private void backtracking() {
+        if (counts[0] == 0 && counts[1] == 0) {
+            output.add(sb.toString());
             return;
         }
 
-        if (open < n) {
-            backtracking(n, open+1, close, str+"(", output);
-        }
+        int opening = counts[0];
+        int closing = counts[1];
 
-        if (close < open) {
-            backtracking(n, open, close+1, str+")", output);
+        if (opening > 0) {
+            sb.append("(");
+            counts[0] -= 1;
+            backtracking();
+            sb.deleteCharAt(sb.length()-1);
+            counts[0] += 1;
+        }
+        if (closing > opening) {
+            sb.append(")");
+            counts[1] -= 1;
+            backtracking();
+            sb.deleteCharAt(sb.length()-1);
+            counts[1] += 1;
         }
     }
 }
