1733e365d34125e45f29d89e2980f29fb788c3b8
Tony <87371627+TonyKim9401@users.noreply.github.com>
2024-09-30 06:02:25 +0900

    Time: 6 ms (86%), Space: 42.5 MB (80.78%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        if (s1.length() > s2.length()) return false;
+
+        int[] s1Count = new int[26];
+        int[] s2Count = new int[26];
+
+        for (char c : s1.toCharArray()) s1Count[c-'a'] += 1;
+
+        int windowSize = s1.length();
+
+        for (int i = 0; i < windowSize; i++) s2Count[s2.charAt(i) - 'a'] += 1;
+        if (Arrays.equals(s1Count, s2Count)) return true;
+
+        for (int i = windowSize; i < s2.length(); i++) {
+            s2Count[s2.charAt(i)-'a'] += 1;
+            s2Count[s2.charAt(i - windowSize) - 'a'] -= 1;
+            if (Arrays.equals(s1Count, s2Count)) return true;
+        }
+        return false;
+    }
+}
