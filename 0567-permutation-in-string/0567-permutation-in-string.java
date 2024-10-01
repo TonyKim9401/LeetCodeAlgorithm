a1c879b7416010a3ecf402b60d52cd08aa4cd629
Tony <87371627+TonyKim9401@users.noreply.github.com>
2024-10-02 02:19:14 +0900

    Time: 4 ms (99.19%), Space: 42.7 MB (61.51%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -5,7 +5,7 @@ class Solution {
         int[] s1Count = new int[26];
         int[] s2Count = new int[26];
 
-        for (char c : s1.toCharArray()) s1Count[c-'a'] += 1;
+        for (char c : s1.toCharArray()) s1Count[c - 'a'] += 1;
 
         int windowSize = s1.length();
 
@@ -13,7 +13,7 @@ class Solution {
         if (Arrays.equals(s1Count, s2Count)) return true;
 
         for (int i = windowSize; i < s2.length(); i++) {
-            s2Count[s2.charAt(i)-'a'] += 1;
+            s2Count[s2.charAt(i) - 'a'] += 1;
             s2Count[s2.charAt(i - windowSize) - 'a'] -= 1;
             if (Arrays.equals(s1Count, s2Count)) return true;
         }
