4fd0f6bb2341b8563e4f80239de7d0a5d97afb77
Tony <87371627+TonyKim9401@users.noreply.github.com>
2024-10-11 02:06:20 +0900

    Time: 19 ms (49.31%), Space: 45.4 MB (24.13%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -0,0 +1,48 @@
+class Solution {
+    public String minWindow(String s, String t) {
+        Map<Character, Integer> map = new HashMap<>();
+
+        for (char c : t.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
+        int required = map.size();
+        int formed = 0;
+
+        int left = 0;
+        int right = 0;
+        int[] ans = {-1, 0, 0};
+
+        Map<Character, Integer> windowCounts = new HashMap<>();
+
+        while (right < s.length()) {
+            char c = s.charAt(right);
+            windowCounts.put(c, windowCounts.getOrDefault(c, 0) + 1);
+
+            if (map.containsKey(c) &&
+                windowCounts.get(c).intValue() == map.get(c).intValue()) {
+                formed += 1;
+            }
+
+            while (left <= right && formed == required) {
+                c = s.charAt(left);
+
+                if (ans[0] == -1 || right - left + 1 < ans[0]) {
+                    ans[0] = right - left + 1;
+                    ans[1] = left;
+                    ans[2] = right;
+                }
+
+                windowCounts.put(c, windowCounts.get(c) - 1);
+                if (map.containsKey(c) &&
+                    windowCounts.get(c).intValue() < map.get(c).intValue()) {
+                    formed -= 1;
+                }
+
+                left += 1;
+            }
+            right += 1;
+        }
+        return ans[0] == -1 ? "" : s.substring(ans[1], ans[2] + 1);
+    }
+}
