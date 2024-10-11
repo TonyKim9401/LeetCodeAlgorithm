10d0768eabd6ad78c63423f8e740437edfd56da5
Tony <87371627+TonyKim9401@users.noreply.github.com>
2024-10-11 11:50:16 +0900

    Time: 5 ms (95.1%), Space: 42.8 MB (45.82%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,22 +1,24 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        if (s1.length() > s2.length()) return false;
+        if (s2.length() < s1.length()) return false;
 
-        int[] s1Count = new int[26];
-        int[] s2Count = new int[26];
+        int[] s1List = new int[26];
+        int[] s2List = new int[26];
 
-        for (char c : s1.toCharArray()) s1Count[c - 'a'] += 1;
+        for (char c : s1.toCharArray()) s1List[c - 'a'] += 1;
 
-        int windowSize = s1.length();
+        int slideWindowSize = s1.length();
 
-        for (int i = 0; i < windowSize; i++) s2Count[s2.charAt(i) - 'a'] += 1;
-        if (Arrays.equals(s1Count, s2Count)) return true;
+        for (int i = 0; i < slideWindowSize; i++) s2List[s2.charAt(i) - 'a'] += 1;
 
-        for (int i = windowSize; i < s2.length(); i++) {
-            s2Count[s2.charAt(i) - 'a'] += 1;
-            s2Count[s2.charAt(i - windowSize) - 'a'] -= 1;
-            if (Arrays.equals(s1Count, s2Count)) return true;
+        if (Arrays.equals(s1List, s2List)) return true;
+
+        for (int i = slideWindowSize; i < s2.length(); i++) {
+            s2List[s2.charAt(i) - 'a'] += 1;
+            s2List[s2.charAt(i - slideWindowSize) - 'a'] -= 1;
+            if (Arrays.equals(s1List, s2List)) return true;
         }
+        
         return false;
     }
 }
