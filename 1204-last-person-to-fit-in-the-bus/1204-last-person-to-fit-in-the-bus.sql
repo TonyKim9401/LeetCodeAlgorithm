ddcde664a04c8c5b9327c867632a3dca95578cff
Tony <87371627+TonyKim9401@users.noreply.github.com>
2024-11-15 10:29:15 +0900

    Time: 2049 ms (14.02%), Space: 0B (100.00%) - LeetHub

---
+++ 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+SELECT person_name 
+FROM Queue q1 
+WHERE 1000 >= ( 
+    SELECT SUM(weight) 
+    FROM Queue q2 
+    WHERE q2.turn <= q1.turn 
+)
+ORDER BY turn 
+DESC LIMIT 1;
