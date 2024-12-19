f25bfd1998babd858e5eb2d3cd6edede8fff47e1
Tony <87371627+TonyKim9401@users.noreply.github.com>
2024-12-19 12:16:37 +0900

    Time: 0 ms (100%), Space: 42.3 MB (6.98%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.java
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -18,15 +18,14 @@ class Solution {
         return dfs(root.left, root.right);
     }
 
-    public boolean dfs(TreeNode left, TreeNode right) {
+    private boolean dfs(TreeNode left, TreeNode right) {
         if (left == null || right == null) {
-            if (left == null && right == null) return true;
-            return false;
+            return left == null && right == null;
         }
 
         if (left.val != right.val) return false;
 
-        return dfs(left.left, right.right)
-            && dfs(left.right, right.left);
+        return dfs(left.left, right.right) &&
+                dfs(left.right, right.left);
     }
 }
