8b79e80786bb2178e8376ad2810f677fd57b7b9b
Tony <87371627+TonyKim9401@users.noreply.github.com>
2024-12-31 23:46:47 -0500

    Time: 147 ms (53.24%), Space: 41.6 MB (65.43%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,52 +1,34 @@
 class Solution {
     public boolean exist(char[][] board, String word) {
-        // Time complexity: O(n * m * 4^k);
-        // -> The size of board: n * m
-        // -> Check 4 directions by the given word's length: 4^k
-        // Space complexity: O(n * m + k)
-        // -> boolean 2D array: n * M
-        // -> recursive max k spaces
+        boolean[][] visit;
 
-        // Mark visited path to do not go back.
-        boolean[][] visit = new boolean[board.length][board[0].length];
-        
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[0].length; j++) {
-                if (wordSearch(i, j, 0, word, board, visit)) return true;
+                visit = new boolean[board.length][board[0].length];
+                if (bfs(visit, board, word, i, j, 0)) return true;
             }
         }
+
         return false;
     }
 
-    private boolean wordSearch(int i, int j, int idx, String word, char[][] board, boolean[][] visit) {
-
-        // When idx checking reach to the end of the length of the word then, return true
+    private boolean bfs(boolean[][] visit, char[][] board, String word, int i, int j, int idx) {
         if (idx == word.length()) return true;
-
-        // Check if i and j are inside of the range
-        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length) return false;
-
-        // Check if the coordinate equals to the charactor value
-        if (board[i][j] != word.charAt(idx)) return false;
+        if (i < 0 || i > board.length - 1 || j < 0 || j > board[0].length - 1) return false;
         if (visit[i][j]) return false;
+        if (board[i][j] != word.charAt(idx)) return false;
 
-        // Mark the coordinate as visited
         visit[i][j] = true;
-
-        // If visited, the target is gonna be the next charactor
         idx += 1;
 
-        // If any direction returns true then it is true
         if (
-            wordSearch(i+1, j, idx, word, board, visit) ||
-            wordSearch(i-1, j, idx, word, board, visit) ||
-            wordSearch(i, j+1, idx, word, board, visit) ||
-            wordSearch(i, j-1, idx, word, board, visit)
+            bfs(visit, board, word, i+1, j, idx) ||
+            bfs(visit, board, word, i-1, j, idx) ||
+            bfs(visit, board, word, i, j+1, idx) ||
+            bfs(visit, board, word, i, j-1, idx)
         ) return true;
 
-        // If visited wrong direction, turns it as false 
         visit[i][j] = false;
-
         return false;
     }
 }
