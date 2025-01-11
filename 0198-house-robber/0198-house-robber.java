af4a07c4ef94c7193f529b704763d20d5004123b
Tony <87371627+TonyKim9401@users.noreply.github.com>
2025-01-11 15:21:27 -0500

    Time: 0 ms (100%), Space: 41.8 MB (7.07%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,16 +1,14 @@
 class Solution {
     public int rob(int[] nums) {
-        int output = 0;
-        int temp = 0;
-        // current 1 2 4 4
-        // output  1 2 4 4
-        // temp.   0 1 2 4
+        int maxMoney = 0;
+        int tempMoney = 0;
+
         for (int i = 0; i < nums.length; i++) {
-            int current = Math.max(output, temp + nums[i]);
-            temp = output;
-            output = current;
+            int currentMoney = Math.max(maxMoney, tempMoney + nums[i]);
+            tempMoney = maxMoney;
+            maxMoney = currentMoney;
         }
 
-        return output;
+        return maxMoney;
     }
 }
