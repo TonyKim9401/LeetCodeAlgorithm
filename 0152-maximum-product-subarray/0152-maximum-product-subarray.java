d0ade794f08cb6b182d1882a3e05fb0180400c2b
Tony <87371627+TonyKim9401@users.noreply.github.com>
2025-02-13 11:20:28 +0900

    Time: 2 ms (56.8%), Space: 44.9 MB (71.54%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -5,6 +5,7 @@ class Solution {
         int maxProduct = nums[0];
 
         for (int i = 1; i < nums.length; i++) {
+            // 해당 값이 0 미만이면 음수로 부호가 바뀜
             if (nums[i] < 0) {
                 int temp = currentMax;
                 currentMax = currentMin;
@@ -14,6 +15,7 @@ class Solution {
             currentMax = Math.max(nums[i], currentMax * nums[i]);
             currentMin = Math.min(nums[i], currentMin * nums[i]);
 
+            // 현재 최대값과 비교하여 값을 갱신
             maxProduct = Math.max(maxProduct, currentMax);
         }
 
