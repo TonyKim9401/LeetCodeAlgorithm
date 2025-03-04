37e647aa9c0fead6d46d4ef9b0cea8f111f27bd5
Tony <87371627+TonyKim9401@users.noreply.github.com>
2025-03-05 06:21:30 +0900

    Time: 1 ms (98.97%), Space: 44.9 MB (76.14%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -1,28 +1,28 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
-        List<int[]> output = new ArrayList<>();
-
         int i = 0;
         int n = intervals.length;
+        List<int[]> output = new ArrayList<>();
 
+        // 그냥 넣기
         while (i < n && intervals[i][1] < newInterval[0]) {
             output.add(intervals[i]);
             i += 1;
         }
 
+        // newInterval을 새로 만들면 사용된 interval들이 하나가 됩니다 :)
         while (i < n && intervals[i][0] <= newInterval[1]) {
-            newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
-            newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+            newInterval[0] = Math.min(intervals[i][0], newInterval[0]);
+            newInterval[1] = Math.max(intervals[i][1], newInterval[1]);
             i += 1;
         }
-
         output.add(newInterval);
 
+        // 그냥 넣기
         while (i < n) {
             output.add(intervals[i]);
             i += 1;
         }
-
         return output.toArray(new int[output.size()][]);
     }
 }
