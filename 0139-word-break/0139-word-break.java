82f56fbbed30c8f0a3d5c7e67faa7418ae7758e8
Tony <87371627+TonyKim9401@users.noreply.github.com>
2025-03-11 04:09:37 +0900

    Time: 9 ms (24.78%), Space: 44.5 MB (47.83%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -1,17 +1,16 @@
-// TC: O(n^2)
-// -> use 2 for-loops to search
-// SC: O(n)
-// -> boolean array's size
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
-        Set<String> set = new HashSet(wordDict);
-
+        // boolean[] = s.length + 1
+        // s = leetcode, wordDict = [leet, code]
+        // i, j
+        // [t,f,f,f,t,f,f,f,f,t]
+        
         boolean[] dp = new boolean[s.length() + 1];
         dp[0] = true;
 
         for (int i = 1; i <= s.length(); i++) {
             for (int j = 0; j < i; j++) {
-                if (dp[j] && set.contains(s.substring(j, i))) {
+                if (dp[j] && wordDict.contains(s.substring(j, i))) {
                     dp[i] = true;
                     break;
                 }
