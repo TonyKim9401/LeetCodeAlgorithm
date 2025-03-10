8541c733325ba03aa071a27bd58351eae7c0c191
Tony <87371627+TonyKim9401@users.noreply.github.com>
2025-03-11 04:09:44 +0900

    Time: 9 ms (24.78%), Space: 44.6 MB (47.83%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -1,21 +1,21 @@
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
-        // boolean[] = s.length + 1
-        // s = leetcode, wordDict = [leet, code]
-        // i, j
-        // [t,f,f,f,t,f,f,f,f,t]
+        // TC: O(n^3)
+        // SC: O(n) n is the length of String s
         
         boolean[] dp = new boolean[s.length() + 1];
         dp[0] = true;
 
-        for (int i = 1; i <= s.length(); i++) {
-            for (int j = 0; j < i; j++) {
-                if (dp[j] && wordDict.contains(s.substring(j, i))) {
+        for (int i = 1; i <= s.length(); i++) { // O(n)
+            for (int j = 0; j < i; j++) { // O(n)
+                if (dp[j] && wordDict.contains(s.substring(j, i))) { // O(n)
                     dp[i] = true;
                     break;
                 }
             }
         }
+
+        // <= 10^6
         return dp[s.length()];
     }
 }
