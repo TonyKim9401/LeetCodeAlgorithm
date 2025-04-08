8ce32d3341bc6b4bd2c247a91f310ec04c6391b5
Tony <87371627+TonyKim9401@users.noreply.github.com>
2025-04-09 08:18:08 +0900

    Time: 6 ms (43.17%), Space: 42.6 MB (99.58%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,19 +1,18 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
         if (s.length() != t.length()) return false;
-        
-        int[] sChar = new int[28];
-        int[] tChar = new int[28];
+
+        int[] countIdx = new int[28];
 
         for (int i = 0; i < s.length(); i++) {
             char sc = s.charAt(i);
             char tc = t.charAt(i);
-            sChar[sc - 'a'] += 1;
-            tChar[tc - 'a'] += 1;
+            countIdx[sc - 'a'] += 1;
+            countIdx[tc - 'a'] -= 1;
         }
 
         for (int i = 0; i < 28; i++) {
-            if (sChar[i] != tChar[i]) return false;
+            if (countIdx[i] != 0) return false;
         }
 
         return true;
