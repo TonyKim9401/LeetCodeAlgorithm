193701118e0a5fa77e71ece5c858084bd62c4b93
Tony <87371627+TonyKim9401@users.noreply.github.com>
2025-04-12 01:18:32 +0900

    Time: 2 ms (88.02%), Space: 55.4 MB (75.43%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -4,12 +4,13 @@ class Solution {
         int[] right = new int[nums.length];
         
         left[0] = 1;
-        for (int i = 1; i < nums.length; i++) {
+        for (int i = 1; i < left.length; i++) {
             left[i] = left[i-1] * nums[i-1];
         }
 
-        right[right.length-1] = 1;
-        for (int i = nums.length - 2; i >= 0; i--) {
+        int idx = right.length - 1;
+        right[idx] = 1;
+        for (int i = idx-1; i >= 0; i--) {
             right[i] = right[i+1] * nums[i+1];
         }
 
