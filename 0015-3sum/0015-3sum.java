7c5d2091fea4dc918e9add3af12e3e7f0e994604
Tony <87371627+TonyKim9401@users.noreply.github.com>
2025-04-13 09:19:07 +0900

    Time: 30 ms (67.93%), Space: 51.9 MB (39.71%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,28 +1,33 @@
-public class Solution {
+class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-
-        Arrays.sort(nums); // Sorted Array
-
         List<List<Integer>> output = new ArrayList<>();
-        Map<Integer, Integer> map = new HashMap<>();
         
-        for (int i = 0; i < nums.length; ++i) map.put(nums[i], i);
-        
-        for (int i = 0; i < nums.length - 2; ++i) {
-            if (nums[i] > 0) break;
-            
-            for (int j = i + 1; j < nums.length - 1; ++j) {
-                int cValue = -1 * (nums[i] + nums[j]);
+        Arrays.sort(nums);
 
-                if (map.containsKey(cValue) && map.get(cValue) > j) {
-                    output.add(List.of(nums[i], nums[j], cValue));
-                }
-                j = map.get(nums[j]);
-            }
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            int j = i + 1;
+            int k = nums.length - 1;
             
-            i = map.get(nums[i]);
+            while (j < k) {
+                int temp = nums[i] + nums[j] + nums[k];
+
+                if (temp == 0) {
+                    output.add(List.of(nums[i], nums[j], nums[k]));
+                    
+                    while (j < k && nums[j] == nums[j + 1]) j += 1;
+                    while (j < k && nums[k] == nums[k - 1]) k -= 1;
+
+                    j += 1;
+                    k -= 1;
+                } else if (temp > 0) {
+                    k -= 1;
+                } else {
+                    j += 1;
+                }
+            }
         }
-        
         return output;
     }
 }
