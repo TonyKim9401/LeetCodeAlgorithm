fc43b30ab6c80e3c14cf2b83d79f317074e974ab
Tony <87371627+TonyKim9401@users.noreply.github.com>
2025-05-01 12:33:07 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2765-longest-alternating-subarray/2765-longest-alternating-subarray.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int alternatingSubarray(int[] nums) {
+        // [2,3,11,12,11]
+        // [1, 8, 1, -1]
+        // 3
+        
+        int[] dp = new int[nums.length - 1];
+
+        for (int i = 1; i < nums.length; i++) {
+            dp[i-1] = nums[i] - nums[i-1];
+        }
+
+        int valid = 1;
+        int output = 0;
+        int max = 0;
+
+        for (int num : dp) {
+            if (num == valid) {
+                output += 1;
+                max = Math.max(output, max);
+                valid *= -1;
+            } else {
+                if (num == 1) {
+                    valid = -1;
+                    output = 1;
+                } else {
+                    valid = 1;
+                    output = 0;
+                }
+            }
+        }
+        
+        return max + 1;
+    }
+}
