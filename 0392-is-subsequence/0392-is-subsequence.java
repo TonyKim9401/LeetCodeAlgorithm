997d81b17591e979bcd7e298f7195af1ca0468e4
Tony <87371627+TonyKim9401@users.noreply.github.com>
2025-05-28 12:39:42 -0400

    Time: 5 ms (4.04%), Space: 42.6 MB (5.3%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.java
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -1,18 +1,21 @@
-class Solution {
+public class Solution {
     public boolean isSubsequence(String s, String t) {
-        
-        if (s.length() == 0) return true;
+        int m = s.length(), n = t.length();
+        int[][] dp = new int[m + 1][n + 1];
 
-        int j = 0;
-        for (int i = 0; i < t.length(); i++) {
-            char tChar = t.charAt(i);
-            char sChar = s.charAt(j);
-            if (tChar == sChar) {
-                j += 1;
+        // build dp table
+        for (int i = 0; i <= m; i++) {
+            for (int j = 0; j <= n; j++) {
+                if (i == 0 || j == 0) {
+                    dp[i][j] = 0;
+                } else if (s.charAt(i - 1) == t.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = dp[i][j - 1];
+                }
             }
-            if (j == s.length()) return true;
         }
 
-        return false;
+        return dp[m][n] == m;
     }
 }
