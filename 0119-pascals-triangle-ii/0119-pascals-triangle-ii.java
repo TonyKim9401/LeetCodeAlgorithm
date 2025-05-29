3872fa3258871aa7dd500190eedaa4bc9069494a
Tony <87371627+TonyKim9401@users.noreply.github.com>
2025-05-28 23:29:58 -0400

    Time: 1 ms (67.7%), Space: 41.6 MB (11.8%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.java
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.java
@@ -1,14 +1,39 @@
 class Solution {
     public List<Integer> getRow(int rowIndex) {
-        List<Integer> output = new ArrayList<>();
+        // rowIndex = 4
+        //    [1, 4, 6, 4, 1]
+        // rowIndex = 5
+        //   [1, 5, 10, 10, 5, 1]
+        // rowIndex = 6
+        // [1, 6, 15, 20, 15, 6, 1]
 
-        output.add(1);
-        long prev = 1;
-        for (int i = 1; i <= rowIndex; i++) {
-            long nextVal = prev * (rowIndex - i + 1) / i;
-            output.add((int)nextVal);
-            prev = nextVal;
+        List<List<Integer>> output = new ArrayList<>();
+
+        List<Integer> inside = new ArrayList<>();
+        inside.add(1); 
+        output.add(inside);
+        if (rowIndex == 0) return output.get(rowIndex);
+
+        inside = new ArrayList<>();
+        inside.add(1);
+        inside.add(1);
+        output.add(inside);
+        if (rowIndex == 1) return output.get(rowIndex);
+
+        for (int i = 2; i <= rowIndex; i++) {
+            inside = new ArrayList<>();
+            inside.add(1);
+            List<Integer> prevInside = output.get(i - 1);
+
+            for (int j = 1; j < i; j++) {
+                int val = prevInside.get(j-1) + prevInside.get(j);
+                inside.add(val);
+            }
+
+            inside.add(1);
+            output.add(inside);
         }
-        return output;
+
+        return output.get(rowIndex);
     }
 }
