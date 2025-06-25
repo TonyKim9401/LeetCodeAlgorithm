756f9862d1a66311716d1d82b0efc27df81aeb8d
Tony <87371627+TonyKim9401@users.noreply.github.com>
2025-06-25 16:11:04 -0400

    Time: 32 ms (93.42%), Space: 55.2 MB (92.65%) - LeetHub

--- 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.java
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.java
@@ -1,50 +1,52 @@
-class TrieNode {
-    TrieNode[] children;
-    boolean isEndOfWord;
+class TreeNode {
 
-    public TrieNode() {
-        children = new TrieNode[26];
-        isEndOfWord = false;
+    TreeNode[] children;
+    boolean isEnd;
+
+    TreeNode() {
+        this.children = new TreeNode[28];
+        this.isEnd = false;
     }
 }
 
 class Trie {
 
-    private TrieNode root;
+    TreeNode root;
 
     public Trie() {
-        root = new TrieNode();
+        this.root = new TreeNode();
     }
     
     public void insert(String word) {
-        TrieNode node = root;
+        TreeNode node = root;
         for (char c : word.toCharArray()) {
             int idx = c - 'a';
             if (node.children[idx] == null) {
-                node.children[idx] = new TrieNode();
+                node.children[idx] = new TreeNode();
             }
             node = node.children[idx];
         }
-        node.isEndOfWord = true;
+        node.isEnd = true;
     }
     
     public boolean search(String word) {
-        TrieNode node = searchPrefix(word);
-        return node != null && node.isEndOfWord;
-    }
-
-    private TrieNode searchPrefix(String word) {
-        TrieNode node = root;
+        TreeNode node = root;
         for (char c : word.toCharArray()) {
             int idx = c - 'a';
-            if (node.children[idx] == null) return null;
+            if (node.children[idx] == null) return false;
             node = node.children[idx];
         }
-        return node;
+        return node.isEnd;
     }
     
     public boolean startsWith(String prefix) {
-        return searchPrefix(prefix) != null;
+        TreeNode node = root;
+        for (char c : prefix.toCharArray()) {
+            int idx = c - 'a';
+            if (node.children[idx] == null) return false;
+            node = node.children[idx];
+        }
+        return true;
     }
 }
 
